1d03da555f58cfd66f84d9a4be163cbaf25fa0d6
Vladislava Mayorova <vladamayor@yandex.ru>
2023-05-02 12:12:20 +0300

    change work_with_database/

--- 2.1-databases/work_with_database/phones/views.py
+++ 2.1-databases/work_with_database/phones/views.py
@@ -9,16 +9,24 @@ def index(request):
 def show_catalog(request):
     template = 'catalog.html'
     phone_obj = Phone.objects.all()
+    sort_value = request.GET.get('sort')
+    if sort_value == 'min_price':
+        phone_obj = phone_obj.order_by('price')
+    elif sort_value == 'max_price':
+        phone_obj = phone_obj.order_by('-price')
+    elif sort_value == 'name':
+        phone_obj = phone_obj.order_by('name')
     context = {
-        'phones': phone_obj
+        'phones': phone_obj,
     }
     return render(request, template, context)
 
 
 def show_product(request, slug):
     template = 'product.html'
-    phone_obj = Phone.objects.filter(slug=slug)
+    phone = Phone.objects.filter(slug=slug)
+    print(phone)
     context = {
-        'phone': phone_obj
+        'phone': phone,
     }
     return render(request, template, context)

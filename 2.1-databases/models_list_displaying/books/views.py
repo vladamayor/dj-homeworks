d17572c645f22ed2df659c805176df63c37e25f5
Vladislava Mayorova <vladamayor@yandex.ru>
2023-05-03 11:08:00 +0300

    change models_list_displaying/

--- 2.1-databases/models_list_displaying/books/views.py
+++ 2.1-databases/models_list_displaying/books/views.py
@@ -1,7 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from books.models import Book
+
+
+def index(request):
+    return redirect('books')
 
 
 def books_view(request):
     template = 'books/books_list.html'
-    context = {}
+    books = Book.objects.all()
+    context = {
+        'books': books,
+    }
+    return render(request, template, context)
+
+def book(request, pub_date):
+    template = 'books/book.html'
+    book = get_object_or_404(Book, pub_date=pub_date)
+    next_page = Book.objects.filter(pub_date__gt = pub_date).order_by('pub_date').first()
+    previous_page = Book.objects.filter(pub_date__lt=pub_date).order_by('-pub_date').first()
+    context = {
+        'book': book,
+        'next_page': next_page,
+        'previous_page': previous_page,
+    }
     return render(request, template, context)
--- 2.1-databases/models_list_displaying/main/urls.py
+++ 2.1-databases/models_list_displaying/main/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from books.views import books_view
+from books.views import books_view, index, book
 
 urlpatterns = [
-    path('', books_view, name='books'),
+    path('books/', books_view, name='books'),
+    path('', index),
+    path('books/<pub_date>/', book, name='book'),
     path('admin/', admin.site.urls),
 ]
